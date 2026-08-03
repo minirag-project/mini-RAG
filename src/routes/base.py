5e1b8f4dca69b4b8f3c1ba13373567b657f0d306
Abdallah Sabry <sabryabdallah37@gmail.com>
2026-08-04 02:13:16 +0300

    testing github actions workflow

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, FastAPI , APIRouter
 import os
 from helpers.config import get_settings, Settings
+from datetime import datetime
 
 
 base_router = APIRouter( prefix = "/api/v1" , tags =['api_v1'])
@@ -10,4 +11,5 @@ async def default(settings: Settings = Depends(get_settings)):
     app_name = settings.APP_NAME
     app_version = settings.APP_VERSION
     app_description = settings.APP_DESCRIPTION
-    return {'message': f"Welcome to the {app_name} API!", 'version': app_version, 'description': app_description}
+    current_date = datetime.now().date()
+    return {'message': f"Welcome to the {app_name} API!", 'version': app_version, 'description': app_description, 'date': current_date}
